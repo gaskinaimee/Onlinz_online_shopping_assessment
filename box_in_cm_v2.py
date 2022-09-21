ab72d5cff9f1bd6d98fc7e3ba691b4234510ebc3
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2022-09-21 13:15:58 +1200

    Tried making another function. Now moving onto next task.

---
+++ box_in_cm_v2.py
@@ -0,0 +1,16 @@
+#The box_in_cm function is a function made to calculate the altogether height, width and depth of the box.
+def box_in_cm(height, width, depth):
+    height_width_depth = []
+
+
+
+
+    print(height_width_depth)
+
+#PUT IN LIST?
+
+height = int(input("What is the height of the box in cm's?"))
+width = int(input("What is the width of the box in cm's?"))
+depth = int(input("What is the depth of the box in cm's?"))
+
+box_in_cm(height, width, depth)
