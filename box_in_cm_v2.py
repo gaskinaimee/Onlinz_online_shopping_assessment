52d08dbe1928352d1979023e3f70339108027ee7
gaskin.aimee <gaskin.aimee@awatapu.school.nz>
2022-10-28 11:40:37 +1300

    Working on refining code.

--- box_in_cm_v2.py
+++ box_in_cm_v2.py
@@ -1,28 +1,25 @@
 #The box_in_cm function is a function made to calculate the altogether height, width and depth of the box.
 def box_in_cm(height, width, depth):
     height_width_depth = []
-
     box_in_cm = True
     while True:
         try:
-            for number in height_width_depth:
-                if number < 5:
-                    print("Sorry, the box must not be under 5cm's.")
-                    break
-                elif number > 100:
-                    print("Sorry, the box must not exceed 100cm's.")
-                else:
-                    height_width_depth.append(height)
-                    height_width_depth.append(width)
-                    height_width_depth.append(depth)
+            if height < 5:
+                print("Sorry, the measurements of the box must not be lower than 5cm's.")
+            else:
+                height_width_depth.append(height)
+                break
+
+            if width < 5:
+                print("Sorry, the measurements of the box must not be lower than 5cm's.")
+            else:
+                height_width_depth.append(width)
+                break
+
+            if depth < 5:
+                print("Sorry, the measurements of the box must not be lower than 5cm's.")
+            else:
+                height_width_depth.append(depth)
+                break
         except ValueError:
-            print("Please enter a number.")
-
-
-#PUT IN LIST?
-
-height = int(input("What is the height of the box in cm's?"))
-width = int(input("What is the width of the box in cm's?"))
-depth = int(input("What is the depth of the box in cm's?"))
-
-box_in_cm(height, width, depth)
+            print("Please enter a valid number.")
